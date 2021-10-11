e4b06c1144edd947b94957385afc6eac716fbd02
filbertfederico <91465463+filbertfederico@users.noreply.github.com>
2021-10-11 18:12:02 +0700

    Update guessing game.py

--- guessing game.py
+++ guessing game.py
@@ -2,12 +2,17 @@ import random
 randomint = random.randint(1, 100)
 
 x = int(input ("Input a number between 1 - 100 :"))
-print (x)
 
-if x < 1 or x > 100 :
-    print ("invalid input")
-else :
-    if x == randomint :
-        print (x,"correct")
-    else :
-        print ("incorect, computer guessed :", randomint) 
+while x != "randomint":
+    if x > 1 or x < 100 :
+        if x == randomint :
+            print ("correct")
+            break
+        elif x < randomint :
+            print ("too low")
+            x = int(input ("Input a number between 1 - 100 :"))
+        elif x > randomint :
+            print ("too high")
+            x = int(input ("Input a number between 1 - 100 :"))
+        else :
+            print ("incorrect input")    
